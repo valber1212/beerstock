42f9badced7b9c6493e88620c57d0df3546a2b56
valber1212 <carvalho.valber@gmail.com>
2021-03-28 11:54:47 -0700

    first commit

---
+++ beerstock/src/test/java/com/digitalinnovation/one/beerstock/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
